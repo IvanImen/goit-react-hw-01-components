146f6f76f3f7d02598769f02b5ca180498782850
IvanImen <vanyagarret@gmail.com>
2023-12-10 01:16:29 +0200

    first_task

---
+++ src/components/App/App.jsx
@@ -0,0 +1,33 @@
+import {
+Profile,
+} from 'components';
+
+import article from 'data/user.json';
+
+export const App = () => {
+  return (
+    <Section>
+      <Container>
+        <Heading marginBottom="50px" textAlign="center">
+          User Profile
+        </Heading>
+        <BlogCard article={article} />
+
+        <Heading marginTop="50px" marginBottom="50px" textAlign="center">
+          Task 2
+        </Heading>
+        <Statistics statsData={statsData} />
+
+        <Heading marginTop="50px" marginBottom="50px" textAlign="center">
+          Task 3
+        </Heading>
+        <ForbesList forbesList={forbesList} />
+
+        <Heading marginTop="50px" marginBottom="50px" textAlign="center">
+          Task 4
+        </Heading>
+        <CryptoHistory transactions={transactions} />
+      </Container>
+    </Section>
+  );
+};
